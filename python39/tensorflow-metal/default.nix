ce795ccd07079c96c0abf0bc59011a77f22d80cc
Matthew Wakefield <matthew.wakefield@wehi.edu.au>
2022-02-22 16:09:39 +1100

    draft tensorflow-metal

---
+++ python39/tensorflow-metal/default.nix
@@ -0,0 +1,46 @@
+# Draft
+# Known Issue: conflict between the tensorboard files
+# with same name and different hash created by macos and metal
+
+let
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.4.0";
+  }) {
+    # optionally bring your own nixpkgs
+    # pkgs = import <nixpkgs> {};
+
+    # optionally specify the python version
+    python = "python39";
+
+    # optionally update pypi data revision from https://github.com/DavHau/pypi-deps-db
+     pypiDataRev = "897a7471aa4e83aab21d2c501e00fee3f440e0fe";
+     pypiDataSha256 = "03gnaq687gg9afb6i6czw4kzr1gbnzna15lfb26f9nszyfq3iyaj";
+  };
+
+  pyEnv = mach-nix.mkPython rec {
+
+    requirements =  ''
+        jupyterlab
+        numpy
+        pandas
+        Cython
+        tensorflow-macos
+        tensorflow-metal
+      '';
+
+    ignoreCollisions = true;
+    
+    #providers.shapely = "sdist,nixpkgs";
+  };
+in
+mach-nix.nixpkgs.mkShell {
+
+  buildInputs = [
+    pyEnv
+  ] ;
+
+  shellHook = ''
+    jupyter lab --notebook-dir=~/
+  '';
+}
